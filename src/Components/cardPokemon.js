af5678677953aee5ac1ee0c64243b4b32d6f015f
yukioz <yukio.link@gmail.com>
2021-02-16 21:57:43 -0300

    Cores de acordo com o tipo do pokemon

--- src/Components/cardPokemon.js
+++ src/Components/cardPokemon.js
@@ -1,46 +1,121 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 
-const CardPokemon = ({pokemon}) => {
+const CardPokemon = ({ pokemon }) => {
   const [showInfo, setShowInfo] = useState(false);
   const [showType, setShowType] = useState(false);
+  const [showColor, setShowColor] = useState("lightblue");
 
 
 
-/*
-  function showType(pokemon){
+  /*
+    function showType(pokemon){
+  
+      faz a mesa coisa que função acima ^^^^
+    }
+  */
 
-    faz a mesa coisa que função acima ^^^^
-  }
-*/
+  useEffect(() => {
+
+    var a = pokemon.type[0]
+
+    switch(a){
+
+      case 'grass':
+        setShowColor("#7FFF00")
+        break;
+
+      case 'Poison':
+        setShowColor("#8A2BE2")
+        break;
+
+      case 'Flying':
+        setShowColor("F0F8FF")
+        break;
+
+      case 'Fire':
+        setShowColor("#DC143C")
+        break;
+
+      case 'Ice':
+        setShowColor("#00BFFF")
+        break;
+
+      case 'Pychic':
+        setShowColor("#FF1493")
+        break;
+
+      case 'Water':
+        setShowColor("#1E90FF")
+        break;
+
+      case 'Electric':
+        setShowColor("#FFD700")
+        break;
+
+      case 'Bug':
+        setShowColor("#F0E68C")
+        break;
+
+      case 'Rock':
+        setShowColor("#808080")
+        break;
+
+      case 'Normal':
+        setShowColor("#D2691E")
+        break;
+
+      case 'Ground':
+        setShowColor("#DEB887")
+        break;
+
+      case 'Fighting':
+        setShowColor("#A52A2A")
+        break;
+
+      case 'Dragon':
+        setShowColor("00008B")
+        break;
+
+      case 'Ghost':
+        setShowColor("#7B68EE")
+        break;
+
+      case 'Dark':
+        setShowColor("#000000")
+        break;
+
+      default:
+        setShowColor("lightblue")
+        break;
+
+      
+    }
+
+
+  }, [])
 
   return (
-    <div style={styles.container} onClick={() => setShowInfo(!showInfo)}>
-        <h1>{pokemon.name}</h1>
 
-        {
-          <img src={pokemon.img} style={{width: 100, height: 100}} alt={pokemon.name} />
-        }
+    <div style={{...styles.container, backgroundColor: showColor}} onClick={() => setShowType(!showType)}>
+      <h1 style={{color: 'black'}}>{pokemon.name}</h1>
 
 
-        <div style={{marginTop: 10}}> 
-          <button onClick={() => setShowType(!showType)} >
-            Type
-          </button>
-        </div>
 
-
-      {showType && 
+      {showType &&
         <div>
           {
-            pokemon.type.map( tipo_do_pokemon => {
+            pokemon.type.map(tipo_do_pokemon => {
 
-              return <h4>{tipo_do_pokemon}</h4>
-              
+              return <h4 style={{color: '#00008B'}}>{tipo_do_pokemon}</h4>
+
             })
           }
-        </div> 
-        
-        }
+        </div>
+
+      }
+
+      <img src={pokemon.img} style={{ width: 100, height: 100 }} alt={pokemon.name} />
+
     </div>
   );
 
@@ -54,15 +129,15 @@ const Dbutton = () => {
 }
 
 const styles = {
-    container: {
-      backgroundColor: "lightblue",
-      display: "flex",
-      justifyContent: 'space-around',
-      margin: 10,
-      width: '30%',
-      height: '30%',
-      borderRadius: 10
-    }
+  container: {
+
+    display: "flex",
+    justifyContent: 'space-around',
+    margin: 10,
+    width: '30%',
+    height: '30%',
+    borderRadius: 10
+  }
 }
 
 export default CardPokemon;
